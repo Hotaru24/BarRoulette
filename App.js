0e56be78fde36626aadb4cca5c9e031e02eaff32
Hotaru24 <florent.pasquet.24@gmail.com>
2019-12-19 01:21:34 +0100

    FindBar Form beginning

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
-import Map from './components/Map'
+import Map from './components/Map';
+import FindBar from './components/FindBar'
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
@@ -9,8 +10,8 @@ const App = () => {
 	return (
     <Fragment>
         
-      <Map/>
-   
+      {/* <Map/> */}
+   <FindBar />
     </Fragment>
 	);
 }
---
+++ components/FindBar.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { Container, Header, Content, Form, Item, Input, Label, Picker } from 'native-base';
+
+const FindBar = () => {
+		return (
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Nom du bar </Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Horaires</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
+    );
+}
+
+export default FindBar;
